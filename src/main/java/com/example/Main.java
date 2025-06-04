7e005357d43b71610f5e9cbe03403639906ef6aa
AdithiDShetty <adithi1138@gmail.com>
2025-06-04 11:47:11 +0530

    initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven and JUnit!");
+    }
+
+    // Example method to test
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
